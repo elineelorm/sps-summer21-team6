f13ef04efc8f42b786593e5ef4dd4b46ab79e985
Eline <elinenuviadenu38@gmail.com>
2021-07-04 09:37:19 +0000

    Update Javascript function

--- app/src/main/webapp/patients.js
+++ app/src/main/webapp/patients.js
@@ -1,31 +1,12 @@
-function on1() {
-  document.getElementById("overlay1").style.display = "block";
-}
-
-function off1() {
-  document.getElementById("overlay1").style.display = "none";
-}
-
-function on2() {
-  document.getElementById("overlay2").style.display = "block";
-}
-
-function off2() {
-  document.getElementById("overlay2").style.display = "none";
-}
-
-function on3() {
-  document.getElementById("overlay3").style.display = "block";
-}
-
-function off3() {
-  document.getElementById("overlay3").style.display = "none";
-}
-
-function on4() {
-  document.getElementById("overlay4").style.display = "block";
-}
-
-function off4() {
-  document.getElementById("overlay4").style.display = "none";
+/**
+ * Function to display the hidden list content
+ * @param {*} tabName 
+ */
+function openTab(tabName) {
+  var i, x;
+  x = document.getElementsByClassName("containerTab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(tabName).style.display = "block";
 }
