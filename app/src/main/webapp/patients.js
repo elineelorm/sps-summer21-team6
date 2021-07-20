8d67f629bb168e81dcd6b609fbb3eb8e689840fe
Eline <elinenuviadenu38@gmail.com>
2021-07-20 22:04:31 +0000

    Remove extra tabs from patient list

--- app/src/main/webapp/patients.js
+++ app/src/main/webapp/patients.js
@@ -1,14 +1,9 @@
 /**
- * Function to display the hidden list content
- * @param {*} tabName 
+ * Function to transfer elements to the volunteers list 
  */
 function openTab(tabName) {
-  var i, container;
-  container = document.getElementsByClassName("containerTab");
-  for (i = 0; i < container.length; i++) {
-    container[i].style.display = "none";
-  }
-  document.getElementById(tabName).style.display = "block";
+ 
+  document.getElementById(tabName).style.display = "none";
 }
 
 async function addPatients(){
